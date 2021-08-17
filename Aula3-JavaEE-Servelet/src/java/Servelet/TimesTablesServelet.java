3eab4306ce356d90c3a515fd0a10fe657a1763ca
Vinicius Pecorari <ovinicius.pecorari@gmail.com>
2021-08-16 21:54:14 -0300

    Aula 3 - Servelets e parametros

---
+++ Aula3-JavaEE-Servelet/src/java/Servelet/TimesTablesServelet.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servelet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Desenvolvedor
+ */
+@WebServlet(name = "TimesTablesServelet", urlPatterns = {"/tabuada.html"})
+public class TimesTablesServelet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>JavaEE Servelets</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h4><a href='index.html'>Voltar ao menu principal</a></h4>");
+            out.println("<h1>JavaEE</h1>");
+            out.println("<h2>Servelets</h2>");
+            out.println("Links rápidos");
+            
+            for(int i = 1; i <=10; i++){
+            out.println("<a href='?n="+i+"'> "+i+" </a> |");
+            }
+            
+            out.println("<h3>Tabuada:</h3>");
+            int n =2;
+            try{
+                n = Integer.parseInt(request.getParameter("n"));
+            }catch(Exception ex){
+                out.println("<p style='color:red;'>Erro ao ler o parâmetro: "+ex.getLocalizedMessage()+"</p>");
+            }
+            
+            out.println("<table");
+            for(int i = 1; i <=10; i++){
+                
+                out.println("<tr>");
+                out.println("<th>2</th>");
+                out.println("<th>x</th>");
+                out.println("<th>"+i+"</th>");
+                out.println("<th> = </th>");
+                out.println("<th>"+(n*i)+"</th>");
+                out.println("</tr>");
+            }
+            
+            out.println("</table>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
